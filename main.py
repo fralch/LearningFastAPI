2e61f62c261ef72dcb3f1fddf62312b8dc3d4d9e
Frank Cairampoma <ingfralch@gmail.com>
2023-08-01 18:31:23 -0500

    path y query

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+# Path; "http://localhost:8000/items/1"
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
+# Query: "http://localhost:8000/items/?item_id=1&q=string"
+@app.get("/items/")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 #iniciar servidor: uvicorn main:app --reload
 # ver documentacion: http://localhost:8000/docs
