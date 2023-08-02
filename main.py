987a82ab95f2ce434eebc94d132fd2cc1c6f0850
Frank Cairampoma <ingfralch@gmail.com>
2023-08-02 18:31:14 -0500

    trabajando routers

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from routers import users
 
 app = FastAPI()
+
+#Routers
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# Path; "http://localhost:8000/items/1"
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-# Query: "http://localhost:8000/items/?item_id=1&q=string"
-@app.get("/items/")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/user/", status_code=201, response_model=User) # response_model=User: para que devuelva el objeto creado
-async def create_user(user: User):
-    return user
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    raise HTTPException(status_code=204, detail="Item ")
-    # return {"item_name": item.name, "item_id": item_id}
-
-@app.patch("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.delete("/items/{item_id}")
-async def delete_item(item_id: int):
-    return {"item_id": item_id}
 
 
 #iniciar servidor: uvicorn main:app --reload
---
+++ routers/users.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+
+# Path; "http://localhost:8000/items/1"
+@router.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+# Query: "http://localhost:8000/items/?item_id=1&q=string"
+@router.get("/items/")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+@router.post("/user/", status_code=201, response_model=User) # response_model=User: para que devuelva el objeto creado
+async def create_user(user: User):
+    return user
+
+@router.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    raise HTTPException(status_code=204, detail="Item ")
+    # return {"item_name": item.name, "item_id": item_id}
+
+@router.patch("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@router.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"item_id": item_id}
+
