f336e2eeedc14f2cb493eacfff5d4c5e89dddeb8
Frank Cairampoma <ingfralch@gmail.com>
2023-08-02 16:55:50 -0500

    post, put, patch, delete

--- main.py
+++ main.py
@@ -16,6 +16,24 @@ async def read_item(item_id: int):
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
+@app.post("/user/")
+async def create_user(user: User):
+    return user
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.patch("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"item_id": item_id}
+    
+
 #iniciar servidor: uvicorn main:app --reload
 # ver documentacion: http://localhost:8000/docs
 # ver documentacion: http://localhost:8000/redoc
