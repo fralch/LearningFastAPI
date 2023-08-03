5d3e35c1e3dbfc494d833ce0b88a148d916c106e
Frank Cairampoma <ingfralch@gmail.com>
2023-08-03 16:28:28 -0500

    corrigiendo bugs

--- routers/users.py
+++ routers/users.py
@@ -14,17 +14,17 @@ async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
-@router.post("/user/", status_code=201, response_model=User) # response_model=User: para que devuelva el objeto creado
-async def create_user(user: User):
+@router.post("/user/", status_code=201) # response_model=User: para que devuelva el objeto creado
+async def create_user(user: str):
     return user
 
 @router.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: str):
     raise HTTPException(status_code=204, detail="Item ")
     # return {"item_name": item.name, "item_id": item_id}
 
 @router.patch("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: str):
     return {"item_name": item.name, "item_id": item_id}
 
 @router.delete("/items/{item_id}")
