ff5b7b643da00c184380776aef7f9b595552052b
Frank Cairampoma <ingfralch@gmail.com>
2023-08-03 17:43:08 -0500

    suportando estaticos

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from routers import users, products
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 #Routers
 app.include_router(users.router)
 app.include_router(products.router)
+app.mount("/imagenes", StaticFiles(directory="static"), name="static") #para que se vean las imagenes la ruta es: http://localhost:8000/imagenes/
+
 
 @app.get("/")
 async def root():
