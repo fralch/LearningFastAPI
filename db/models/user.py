754ab29b629d62e5a28c95c5ef4d858aaf619aec
Frank Cairampoma <ingfralch@gmail.com>
2023-08-22 17:09:58 -0500

    creando usuario

--- db/models/user.py
+++ db/models/user.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
+from typing import Optional
 
-
-class UserModel (BaseModel):
-    id: str | None
+class UserModel(BaseModel):
+    id: str = None  # Haciendo que id tenga un valor predeterminado de None
     username: str
     email: str
--- routers/users_db.py
+++ routers/users_db.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from db.models.user import UserModel as User
 from db.cliente import db_client
 
-router = APIRouter( tags=["users_db"]  )
+router = APIRouter( prefix="/user_db", tags=["users_db"]  )
 
 
 
@@ -12,13 +12,38 @@ async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
-@router.post("/crear",response_model=User , status_code=201) 
+@router.post("/") 
 async def create_user(user: User):
+    # return user.dict()
     user_dict = dict(user)
     del user_dict["id"]
     id = db_client.local.users.insert_one(user_dict).inserted_id
     
     new_user = db_client.local.users.find_one({"_id": id})
+    #sacando el id de la respuesta
+    id_mongo = str(new_user["_id"])
+    #remplazando el id de mongo por el id de la respuesta 
+    """
+     Es como cuando se sobre escribe un valor de un objeto en javascript, pero en un diccionario de python
+        JAVASCRIPT
+        const myObject = {
+        name: "John Doe",
+        age: 30
+        };
+
+        myObject.name = "Jane Doe";
+
+        PYTHON
+        my_dict = {
+        "name": "John Doe",
+        "age": 30
+        }
+
+        my_dict["name"] = "Jane Doe"
+
+    """
+    new_user["_id"] =  id_mongo
+
     return new_user
 
 @router.put("/{id}")
