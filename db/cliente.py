9011a0a8c42655487e79e910789092144f008f53
Frank Cairampoma <ingfralch@gmail.com>
2023-08-23 11:07:05 -0500

    subien env de mongo atlas

--- db/cliente.py
+++ db/cliente.py
@@ -1,6 +1,10 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv() # Carga las variables de entorno desde el archivo .env
 # DBLocal
 # db_client = MongoClient('localhost', 27017).local 
 
 # DBAtlas
+db_client = MongoClient(os.environ["MONGO_ATLAS"]).test
